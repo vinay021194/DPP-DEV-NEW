6c4d7c72a041a26d9ed5fc5175f971cec7e5bfd4
pankaj-404 <pankajbhayre@gmail.com>
2022-09-05 14:53:32 +0530

    remove unwanted code and fix plants vlues

--- src/components/AppMenu.jsx
+++ src/components/AppMenu.jsx
@@ -4,39 +4,31 @@ import { Button } from "primereact/button";
 import { AutoComplete } from "primereact/autocomplete";
 import { ProductService } from "../services/ProductService";
 
-
 export const AppMenu = (props) => {
   const [cities, setCities] = useState([]);
   const [selectedMaterial, setSelectedMaterial] = useState(null);
   const [selectPlant, setSelectPlant] = useState(null);
   const [filteredMaterial, setFilteredMaterial] = useState(null);
   const [filteredPlant, setfilteredPlant] = useState(null);
-  const[materials,setMaterials]= useState(null)
-  const[plants,setPlants]= useState(null)
-  const [uniqPlant, setUniqPlant] = useState([]);
+  const [materials, setMaterials] = useState(null);
+  const [plants, setPlants] = useState(null);
   const productService = new ProductService();
 
-  useEffect(()=>{
+  useEffect(() => {
     productService.getMaterialInfo().then((data) => {
       const uniq = (items) => [...new Set(items)];
       const uniqMaterial = uniq(data.map((item) => item.material.toString()));
-      console.log("uniqMaterial",uniqMaterial)
-      // const key = 'age';
-
-//  const uniqMaterial = [...new Map(data.map(item =>
-//   [item[key], item])).values()];
       return setMaterials(uniqMaterial);
-      
     });
     productService.getMaterialInfo().then((data) => {
       const uniq = (items) => [...new Set(items)];
-      const uniqPlants = uniq(data.map((item) => item.plant.toString()));
-      console.log("uniqPlants",uniqPlants)
+      let uniqPlants = data.map((item) => item.expend);
+      uniqPlants = [].concat(...uniqPlants);
+      uniqPlants = uniq(uniqPlants.map((item) => item.plant.toString()));
+      uniqPlants.sort();
       return setPlants(uniqPlants);
-      
     });
-    
-  },[])
+  }, []);
 
   const onCityChange = (e) => {
     let selectedCities = [...cities];
@@ -44,8 +36,7 @@ export const AppMenu = (props) => {
       selectedCities = [];
     } else if (e.target.value === "All" && e.checked)
       selectedCities = ["All", "Urgent", "Depleting fast", "Sufficient"];
-    else if (e.target.value === "All" && e.checked !== true)
-      selectedCities = [];
+    else if (e.target.value === "All" && e.checked !== true) selectedCities = [];
     else if (e.checked) selectedCities.push(e.value);
     else selectedCities.splice(selectedCities.indexOf(e.value), 1);
 
@@ -60,8 +51,7 @@ export const AppMenu = (props) => {
 
   const onPlantChange = (e) => {
     setSelectPlant(e.value);
-    props.handlefilter(e.value,"singleselect");
-    console.log('handlePlantfilter',props)
+    props.handlefilter(e.value, "singleselect");
   };
 
   const searchMaterial = (event) => {
@@ -70,11 +60,12 @@ export const AppMenu = (props) => {
       if (!event.query.trim().length) {
         _filteredMaterial = [...materials];
       } else {
-        _filteredMaterial = materials.filter((item) =>{
-         return item.material})};
+        _filteredMaterial = materials.filter((item) => {
+          return item.material;
+        });
+      }
       setFilteredMaterial(_filteredMaterial);
-    }, );
-      
+    });
   };
 
   const searchPlant = (event) => {
@@ -87,7 +78,7 @@ export const AppMenu = (props) => {
       }
 
       setfilteredPlant(_filteredPlant);
-    }, );
+    });
   };
 
   return (
@@ -103,99 +94,100 @@ export const AppMenu = (props) => {
         <h5>Filter By</h5>
       </div>
       <hr />
-      <div style={{ marginLeft: "25px", fontFamily: "Poppins" }}>
-        <strong>Material Status</strong>
-      </div>
-      <div className="gridcheck">
-        <Checkbox
-          inputId="cb4"
-          value="All"
-          onChange={onCityChange}
-          checked={cities.includes(
-            "All",
-            "Urgent",
-            "Depleting fast",
-            "Sufficient"
-          )}
-          style={{ marginRight: "15px" }}
-        ></Checkbox>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <div style={{ margin: "10px" }}>
+          <div style={{ fontFamily: "Poppins" }}>
+            <strong>Material Status</strong>
+          </div>
+          <div className="gridcheck">
+            <Checkbox
+              inputId="cb4"
+              value="All"
+              onChange={onCityChange}
+              checked={cities.includes("All", "Urgent", "Depleting fast", "Sufficient")}
+              style={{ marginRight: "15px" }}
+            ></Checkbox>
 
-        <label htmlFor="cb4" className="p-checkbox-label">
-          All
-        </label>
-      </div>
-      <div className="gridcheck">
-        <Checkbox
-          inputId="cb1"
-          value="Urgent"
-          onChange={onCityChange}
-          checked={cities.includes("Urgent")}
-          style={{ marginRight: "15px" }}
-        ></Checkbox>
-        <label htmlFor="cb1" className="p-checkbox-label">
-          Urgent
-        </label>
-      </div>
-      <div className="gridcheck">
-        <Checkbox
-          inputId="cb2"
-          value="Depleting fast"
-          onChange={onCityChange}
-          checked={cities.includes("Depleting fast")}
-          style={{ marginRight: "15px" }}
-        ></Checkbox>
-        <label htmlFor="cb2" className="p-checkbox-label">
-          Depleting fast
-        </label>
-      </div>
-      <div className="gridcheck">
-        <Checkbox
-          inputId="cb3"
-          value="Sufficient"
-          onChange={onCityChange}
-          checked={cities.includes("Sufficient")}
-          style={{ marginRight: "15px" }}
-        ></Checkbox>
-        <label htmlFor="cb3" className="p-checkbox-label">
-          Sufficient
-        </label>
-      </div>
-      <div className="gridcheck">
-        <Button
-          className="nextbutton1"
-          label="Clear All "
-          value="clear"
-          onClick={onCityChange}
-        />
-      </div>
-      <hr />
-      <div style={{ marginLeft: "25px", fontFamily: "Poppins" }}>
-        <strong>Material ID</strong>
-      </div>
-      <div className="gridcol">
-        <AutoComplete
-          value={selectedMaterial}
-          suggestions={filteredMaterial}
-          completeMethod={searchMaterial}
-          dropdown
-         // field="material"
-          multiple
-          onChange={onMaterialChange}
-          aria-label="materials"
-          sorted
-        />
-      </div>
-      <div className="gridcol">
-        <AutoComplete
-          value={selectPlant}
-          suggestions={filteredPlant}
-          completeMethod={searchPlant}
-          dropdown
-          // field="plant"
-          onChange={onPlantChange}
-          aria-label="plants"
-          sorted
-        />
+            <label htmlFor="cb4" className="p-checkbox-label">
+              All
+            </label>
+          </div>
+          <div className="gridcheck">
+            <Checkbox
+              inputId="cb1"
+              value="Urgent"
+              onChange={onCityChange}
+              checked={cities.includes("Urgent")}
+              style={{ marginRight: "15px" }}
+            ></Checkbox>
+            <label htmlFor="cb1" className="p-checkbox-label">
+              Urgent
+            </label>
+          </div>
+          <div className="gridcheck">
+            <Checkbox
+              inputId="cb2"
+              value="Depleting fast"
+              onChange={onCityChange}
+              checked={cities.includes("Depleting fast")}
+              style={{ marginRight: "15px" }}
+            ></Checkbox>
+            <label htmlFor="cb2" className="p-checkbox-label">
+              Depleting fast
+            </label>
+          </div>
+          <div className="gridcheck">
+            <Checkbox
+              inputId="cb3"
+              value="Sufficient"
+              onChange={onCityChange}
+              checked={cities.includes("Sufficient")}
+              style={{ marginRight: "15px" }}
+            ></Checkbox>
+            <label htmlFor="cb3" className="p-checkbox-label">
+              Sufficient
+            </label>
+          </div>
+          <div className="gridcheck p-mt-3">
+            <Button className="previousbutton" label="Clear All " value="clear" onClick={onCityChange} />
+          </div>
+        </div>
+        <hr />
+        <div style={{ margin: "10px" }}>
+          <div>
+            <strong>Material ID</strong>
+          </div>
+          <div className="">
+            <AutoComplete
+              value={selectedMaterial}
+              suggestions={filteredMaterial}
+              completeMethod={searchMaterial}
+              dropdown
+              multiple
+              onChange={onMaterialChange}
+              aria-label="materials"
+              sorted
+            />
+          </div>
+        </div>
+        <hr />
+        <div style={{ margin: "10px" }}>
+          <div>
+            <strong>Plant ID</strong>
+          </div>
+          <div className="">
+            <AutoComplete
+              value={selectPlant}
+              suggestions={filteredPlant}
+              completeMethod={searchPlant}
+              dropdown
+              // field="plant"
+              onChange={onPlantChange}
+              aria-label="plants"
+              sorted
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/MaterialOverview.jsx
+++ src/components/MaterialOverview.jsx
@@ -14,7 +14,6 @@ export const MaterialOverview = (props) => {
   const [productsFiltered, setproductsFiltered] = useState([]);
   const [displayBasic, setDisplayBasic] = useState(false);
 
-  const [expandedRows, setExpandedRows] = useState(null);
   const isMounted = useRef(false);
   const productService = new ProductService();
   const [layoutMode, setLayoutMode] = useState("static");
@@ -24,11 +23,6 @@ export const MaterialOverview = (props) => {
   const [mobileMenuActive, setMobileMenuActive] = useState(false);
   const [selectedPlant, setSelectedPlant] = useState(null);
   let menuClick = false;
-
-  useEffect(() => {
-    if (isMounted.current) {
-    }
-  }, [expandedRows]);
 
   useEffect(() => {
     delete localStorage.source;
@@ -41,14 +35,11 @@ export const MaterialOverview = (props) => {
     delete localStorage.endDate;
     delete localStorage.suppliers;
 
-    // localStorage.clear();
     isMounted.current = true;
-    // productService.getProductsWithOrdersSmall().then(data => setProducts(data));
     productService.getMaterialInfo().then((data) => {
       return setProducts(data);
     });
     productService.getMaterialInfo().then((data) => {
-      console.table(data);
       return setproductsFiltered(data);
     });
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
@@ -63,7 +54,6 @@ export const MaterialOverview = (props) => {
       else if (layoutMode === "overlay") return overlayMenuActive;
       else return true;
     }
-
     return true;
   };
 
@@ -77,36 +67,14 @@ export const MaterialOverview = (props) => {
   });
 
   const sidebar = useRef();
-  const history = useHistory();
-  const logo =
-    layoutColorMode === "dark"
-      ? "/assets/layout/images/logo-white.svg"
-      : "/assets/layout/images/logo.svg";
 
   const wrapperClass = classNames("layout-wrapper", {
     "layout-overlay": layoutMode === "overlay",
     "layout-static": layoutMode === "static",
-    "layout-static-sidebar-inactive":
-      staticMenuInactive && layoutMode === "static",
-    "layout-overlay-sidebar-active":
-      overlayMenuActive && layoutMode === "overlay",
+    "layout-static-sidebar-inactive": staticMenuInactive && layoutMode === "static",
+    "layout-overlay-sidebar-active": overlayMenuActive && layoutMode === "overlay",
     "layout-mobile-sidebar-active": mobileMenuActive,
   });
-
-  const onToggleMenu = (event) => {
-    menuClick = true;
-
-    if (isDesktop()) {
-      if (layoutMode === "overlay") {
-        setOverlayMenuActive((prevState) => !prevState);
-      } else if (layoutMode === "static") {
-        setStaticMenuInactive((prevState) => !prevState);
-      }
-    } else {
-      setMobileMenuActive((prevState) => !prevState);
-    }
-    event.preventDefault();
-  };
 
   const onWrapperClick = (event) => {
     if (!menuClick) {
@@ -133,9 +101,7 @@ export const MaterialOverview = (props) => {
 
   const statusOrderBodyTemplate = (rowData) => {
     return (
-      <span
-        className={`productsss-badge status-${rowData.status_level_plant.toLowerCase()}`}
-      >
+      <span className={`productsss-badge status-${rowData.status_level_plant.toLowerCase()}`}>
         {rowData.status_level_plant}
       </span>
     );
@@ -143,9 +109,7 @@ export const MaterialOverview = (props) => {
 
   const statusBodyTemplate = (rowData) => {
     return (
-      <span
-        className={`productss-badge status-${rowData.status_level_material.toLowerCase()}`}
-      >
+      <span className={`productss-badge status-${rowData.status_level_material.toLowerCase()}`}>
         {rowData.status_level_material}
       </span>
     );
@@ -190,26 +154,17 @@ export const MaterialOverview = (props) => {
           <Column selectionMode="single" />
           <Column field="plant" header="Plant ID" />
           <Column field="plant_name" header="Plant Name" />
-          <Column
-            field="status_level_plant"
-            header="Status"
-            body={statusOrderBodyTemplate}
-          />
+          <Column field="status_level_plant" header="Status" body={statusOrderBodyTemplate} />
         </DataTable>
         <div style={{ display: "flex", justifyContent: "center" }}></div>
       </div>
     );
   };
 
-  const handlefilter = (filters=[], types) => {
-
-
-    
+  const handlefilter = (filters = [], types) => {
     if (types === "Multiselect") {
       if (filters.length > 0) {
-        let filteredData = products.filter((data) => {
-          return filters.includes(data.material);
-        });
+        let filteredData = products.filter((data) => filters.includes(data.material));
         setproductsFiltered(filteredData);
       } else if (filters.length > 0) {
       } else {
@@ -217,9 +172,7 @@ export const MaterialOverview = (props) => {
       }
     } else {
       if (filters.length > 0) {
-        let filteredData = products.filter((data) => {
-          return filters.includes(data.status_level_material);
-        });
+        let filteredData = products.filter((data) => filters.includes(data.status_level_material));
         setproductsFiltered(filteredData);
       } else {
         setproductsFiltered(products);
@@ -227,81 +180,13 @@ export const MaterialOverview = (props) => {
     }
     if (types === "singleselect") {
       if (filters.length > 0) {
-        let filteredData = products.filter((data) => {
-          console.log('filteredData',data.expend.plant) 
-          return filters.includes(data.plant);
-          
-        });
-        console.log('filteredData',filteredData) 
+        let filteredData = products.filter((data) => filters.includes(data.plant));
         setproductsFiltered(filteredData);
       } else {
         setproductsFiltered(products);
       }
     }
   };
-
-
-
-
-  // const handlePlantfilter = (filters, types) => {
-  //   if (types === "Multiselect") {
-
-  //     if (filters.length > 0) {
-  //       let filteredData = products.filter((data) => {
-  //         return filters.includes(data.plant);
-  //       });
-  //       setproductsFiltered(filteredData);
-  //     }
-
-  //     else {
-  //       setproductsFiltered(products);
-  //     }
-  //   }
-  // //   else if(types === "singleselect"){
-  // //     // console.log('singleselect',)
-  // //     // if(filters.length > 0){
-  // //     //  console.log('filters',)
-  // //     //  let allMaterial = filters.map((d) => d.name);
-  // //     //  console.log('allMaterial',allMaterial)
-  // //      if (filters) {
-  // //        let filteredData = products.filter((data) => {
-  // //         // console.log('filteredData',)
-  // //          return filters.includes(data.expend.plant);
-  // //        });
-  // //        setproductsFiltered(filteredData);
-
-  // //    }
-  // //  }
-
-  // };
-
-  // const handlePlantfilter = (filters=[], types) => {
-  //   if (types === "singleselect") {
-  //     if (filters.length > 0) {
-  //       let allPlant = filters.map((d) => d.name);
-  //       if (filters) {
-  //         let filteredPlantData = products.filter((data) => {
-  //           console.log('filteredPlantData',data.material)
-  //           return allPlant.includes(data.material);
-
-  //         });
-  //         setproductsFiltered(filteredPlantData);
-  //         //console.log('filteredPlantData',allMaterial)
-  //       }
-  //     } else {
-  //       setproductsFiltered(products);
-  //     }
-  //   } else {
-  //     if (filters.length > 0) {
-  //       let filteredPlantData = products.filter((data) => {
-  //         return filters.includes(data.status_level_material);
-  //       });
-  //       setproductsFiltered(filteredPlantData);
-  //     } else {
-  //       setproductsFiltered(products);
-  //     }
-  //   }
-  // };
 
   return (
     <div className={wrapperClass} onClick={onWrapperClick}>
@@ -326,15 +211,8 @@ export const MaterialOverview = (props) => {
             <Column expander style={{ width: "3em" }} />
             <Column field="material" header="ID"></Column>
             <Column field="material_description_1" header="Name"></Column>
-            <Column
-              field="inventory_material_level"
-              header=" Total Inventory (T)"
-            />
-            <Column
-              field="status_level_material"
-              header="Status"
-              body={statusBodyTemplate}
-            />
+            <Column field="inventory_material_level" header=" Total Inventory (T)" />
+            <Column field="status_level_material" header="Status" body={statusBodyTemplate} />
           </DataTable>
         </div>
         <Button
@@ -355,15 +233,8 @@ export const MaterialOverview = (props) => {
         in={isSidebarVisible()}
         unmountOnExit
       >
-        <div
-          ref={sidebar}
-          className={sidebarClassName}
-          onClick={onSidebarClick}
-        >
-          <AppMenu
-            handlefilter={(filters, types) => handlefilter(filters, types)}
-            // handlePlantfilter={(filters, types) => handlePlantfilter(filters, types)}
-          />
+        <div ref={sidebar} className={sidebarClassName} onClick={onSidebarClick}>
+          <AppMenu handlefilter={(filters, types) => handlefilter(filters, types)} />
         </div>
       </CSSTransition>
     </div>
